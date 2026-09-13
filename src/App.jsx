46fa48ebcbbc7a44858040eebbd369e004c58df7
Meshella Samura <samurameshella38956@gmail.com>
2026-09-13 20:04:26 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Practice from './pages/Practice'
+import Results from './pages/Results'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <main className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/practice" element={<Practice />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
