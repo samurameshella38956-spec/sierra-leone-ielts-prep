51dd0094c7b8df4f0c8d4d16994a82473592ea4c
Meshella Samura <samurameshella38956@gmail.com>
2026-09-13 18:11:32 +0000

    Create models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Progress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    exam_type = db.Column(db.String(50))
+    score = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {"exam_type": self.exam_type, "score": self.score}
