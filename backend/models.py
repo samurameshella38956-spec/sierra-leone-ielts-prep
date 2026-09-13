b2bd117cf1fff41816ed0d07063243136d7b9a43
Meshella Samura <samurameshella38956@gmail.com>
2026-09-13 18:32:13 +0000

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_premium = db.Column(db.Boolean, default=False)
+
+class Progress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    exam_type = db.Column(db.String(50))
+    score = db.Column(db.Integer)
