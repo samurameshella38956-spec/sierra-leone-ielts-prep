ce25d76d5b71a3d331acc08e0903db098f973274
Meshella Samura <samurameshella38956@gmail.com>
2026-09-13 17:23:04 +0000

    Add Navbar component for navigation

---
+++ src/src/src/pages/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex justify-between">
+      <h1 className="font-bold">IELTS Prep</h1>
+      <div className="space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/practice">Practice</Link>
+        <Link to="/results">Results</Link>
+      </div>
+    </nav>
+  )
+}
