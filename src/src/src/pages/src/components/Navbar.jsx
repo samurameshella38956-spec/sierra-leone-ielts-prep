ba101bd11df568465a2b05aa48227783216c9c3a
Meshella Samura <samurameshella38956@gmail.com>
2026-09-14 09:05:08 +0000

    Change Navbar title from 'IELTS Prep' to 'IELTS'

--- src/src/src/pages/src/components/Navbar.jsx
+++ src/src/src/pages/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
-      <h1 className="font-bold">IELTS Prep</h1>
+      <h1 className="font-bold">IELTS</h1>
       <div className="space-x-4">
         <Link to="/">Home</Link>
         <Link to="/practice">Practice</Link>
